3f953325a7a84404dad71c6bf1255736a0f6c6fa
김성현 <gkska741@kaist.ac.kr>
2021-09-16 08:30:51 +0900

    0915수정

--- week2(dp)/0915WED/김성현_2579_계단오르기_S3.py
+++ week2(dp)/0915WED/김성현_2579_계단오르기_S3.py
@@ -9,7 +9,7 @@ max_score = [0] * (N+1)
 if len(stair_value) > 2:
     for i in range(len(stair_value)):
         max_score[1] = stair_value[1]
-        max_score[2] = stair_value[1] + stair_value[2]
+        max_score[2] = stair_value[1] + stair_value[2] #초기값
 else:
     print(stair_value[1])
 
@@ -19,7 +19,8 @@ def stair(n):
         return max_score[n]
     else:
         for i in range(3, n+1):
-            max_score[i] = (max(max_score[i-2], max_score[i-3] + stair_value[i-1])+stair_value[i])
+            max_score[i] = (max(max_score[i-2], max_score[i-3] + stair_value[i-1])+stair_value[i]) #dp 알고리즘
+            
         return max_score[n]
     
 if N > 1:
