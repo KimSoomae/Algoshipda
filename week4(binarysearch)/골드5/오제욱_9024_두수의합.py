527a45dbb511b1a9183fa80cee67f55a148149c6
jeukoh <jeukoh@gmail.com>
2021-10-04 00:47:48 +0900

    1004MON

---
+++ week4(binarysearch)/골드5/오제욱_9024_두수의합.py
@@ -0,0 +1,26 @@
+import sys; readline = sys.stdin.readline
+anw = []
+for _ in range(int(input())):
+    N, K = map(int,readline().split())
+    arr = list(map(int,readline().split()))
+    arr.sort()
+    min_inteval = float('inf')
+    cnt = 0
+    s = 0
+    e = N-1
+    while s != e:
+        tmp = arr[s] + arr[e]
+        if abs(K-tmp) < min_inteval:
+            min_inteval = abs(K-tmp)
+            cnt = 1
+        elif abs(K-tmp) == min_inteval:
+            cnt += 1
+        if tmp > K:
+            e -= 1
+        elif tmp == K:
+            e -= 1
+            s += 1
+        else:
+            s += 1
+    anw.append(cnt)
+print('\n'.join(str(x) for x in anw))
