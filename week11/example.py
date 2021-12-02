6afaed7982c3fe06fbad06fbcd4b886ab9cdd9e1
김성현 <gkska741@kaist.ac.kr>
2021-12-02 13:34:04 +0900

    수정

--- week11/example.py
+++ week11/example.py
@@ -9,12 +9,12 @@ node_value = [0] + list(map(int, input().split()))
 
 
 def dfs(s, day):
-    global value, answer, endposition
+
+    global value, answer
 
     if sum(visited) == N+1:
-        if answer > value:
-            answer = value
-            endposition = s 
+        if answer > value+backcost[s]:
+            answer = value+backcost[s]
             return
 
     for i in range(N+1):
@@ -22,8 +22,8 @@ def dfs(s, day):
 
             value += ( 1 + node_value[i] * day)
             visited[i] = 1
-
-            dfs(i, day+1)
+            if value < answer:
+                dfs(i, day+1)
 
             value -= ( 1 + node_value[i] * day)
             visited[i] = 0
@@ -33,35 +33,42 @@ for _ in range(N+1):
     for j in range(1, len(info)):
         board[info[0]][info[j]] = 1
 
-def bfs(endposition):
-
+def make_back_cost():
+    backcost[0] = 0
+    visited = [1] + [0]*(N)
+    day = 1
     Q = deque()
-    Q.append(endposition)
-    day = 0
+    Q.append(0)
 
     while Q:
+
         temp = []
 
         while Q:
             s = Q.pop()
             for i in range(N+1):
-                if board[s][i] == 1:
+                if board[s][i] == 1 and not visited[i]:
+                    backcost[i] = day
+                    visited[i] = 1
                     temp.append(i)
-        if 0 in temp:
-            return day + 1
         
-        day += 1
-
         for node in temp:
             Q.append(node)
+        day += 1
+        
+
+
 
 visited = [1] + [0] * (N)
+backcost = [0] * (N+1)
+# i번째 노드에서 집까지 가는데 필요한 비용
 
 answer = 0xffffff
 value = 0
 endposition = 0
+make_back_cost()
 dfs(0, 1)
-print(answer, endposition)
-visited = [0] * (N+1)
-returnday = bfs(endposition)
-print(answer + returnday)
+
+print(answer)
+
+
