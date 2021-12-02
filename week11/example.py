09f443f05cfd7d18615151e9db7679da2dd482b4
김성현 <gkska741@kaist.ac.kr>
2021-12-02 14:38:20 +0900

    final

--- week11/example.py
+++ week11/example.py
@@ -3,15 +3,9 @@ from collections import deque
 sys.setrecursionlimit(10**7)
 
 input = open('input.txt').readline
-N, V = map(int, input().split())
-board = [[0]*(N+1) for _ in range(N+1)]
-node_value = [0] + list(map(int, input().split()))
-
 
 def dfs(s, day):
-
     global value, answer
-
     if sum(visited) == N+1:
         if answer > value+backcost[s]:
             answer = value+backcost[s]
@@ -28,10 +22,6 @@ def dfs(s, day):
             value -= ( 1 + node_value[i] * day)
             visited[i] = 0
     
-for _ in range(N+1):
-    info = list(map(int, input().split()))
-    for j in range(1, len(info)):
-        board[info[0]][info[j]] = 1
 
 def make_back_cost():
     backcost[0] = 0
@@ -39,11 +29,8 @@ def make_back_cost():
     day = 1
     Q = deque()
     Q.append(0)
-
     while Q:
-
         temp = []
-
         while Q:
             s = Q.pop()
             for i in range(N+1):
@@ -51,24 +38,34 @@ def make_back_cost():
                     backcost[i] = day
                     visited[i] = 1
                     temp.append(i)
-        
         for node in temp:
             Q.append(node)
         day += 1
         
+TC = int(input())
+for case_number in range(1, TC+1):
+    N, V = map(int, input().split())
+    board = [[0]*(N+1) for _ in range(N+1)]
+    node_value = [0] + list(map(int, input().split()))
+
+    for _ in range(N+1):
+        info = list(map(int, input().split()))
+        for j in range(1, len(info)):
+            board[info[0]][info[j]] = 1
 
 
 
-visited = [1] + [0] * (N)
-backcost = [0] * (N+1)
-# i번째 노드에서 집까지 가는데 필요한 비용
+    visited = [1] + [0] * (N)
+    backcost = [0] * (N+1)
+    answer = 0xffffff
+    value = 0
+    endposition = 0
+    make_back_cost()
+    dfs(0, 1)
 
-answer = 0xffffff
-value = 0
-endposition = 0
-make_back_cost()
-dfs(0, 1)
-
-print(answer)
+    if answer == 0xffffff:
+        print(f'#{case_number} -1' )
+    else:
+        print(f'#{case_number} {answer}')
 
 
